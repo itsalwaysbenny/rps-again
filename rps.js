68f2977446affc362728598c2366fb7ec2a6136d
Ben Taylor <taylor.ben89@gmail.com>
2022-02-09 21:16:09 +0000

    Made and tested a playGame function Returns player and computer selections and who is the winner

--- rps.js
+++ rps.js
@@ -17,19 +17,28 @@ function computerSelection() {
   };
 };
 
-module.exports = computerSelection
-
-function playerSelection(selection) {
-  let playerChoice = selection.toLowerCase();
-  if (playerChoice === "rock") {
-    return playerChoice;
-  } else if (playerChoice === "paper") {
-    return playerChoice;
-  } else if (playerChoice === "scissors") {
-    return playerChoice;
-  } else {
-    return "Please select either 'Rock', 'Paper' or 'Scissors'";
+function playGame(playerSelection, computerSelection) {
+  let computerChoice = computerSelection.toLowerCase();
+  let playerChoice = playerSelection.toLowerCase();
+  if (playerChoice === computerChoice) {
+    return "Game is a Draw";
+  } else if ((playerChoice === "rock") && (computerChoice === "scissors")) {
+      return "Player chose Rock Computer chose Scissors Player Wins";
+  } else if ((playerChoice === "scissors") && (computerChoice === "paper")) {
+      return "Player chose Scissors Computer chose Paper Player Wins"
+  } else if ((playerChoice === "paper") && (computerChoice === "rock")) {
+      return "Player chose Paper Computer chose Rock Player Wins"
+  } else if ((playerChoice === "scissors") && (computerChoice === "rock")) {
+    return "Player chose Scissors Computer chose Rock Computer Wins";
+} else if ((playerChoice === "paper") && (computerChoice === "scissors")) {
+    return "Player chose Paper Computer chose Scissors Computer Wins"
+} else if ((playerChoice === "rock") && (computerChoice === "paper")) {
+    return "Player chose Rock Computer chose Paper Computer Wins"
+} else {
+    return "Please select either 'Rock', 'Paper' or 'Scissors'"
   }
 };
 
-module.exports = playerSelection
+
+module.exports = computerSelection
+module.exports = playGame
--- rps.test.js
+++ rps.test.js
@@ -1,4 +1,5 @@
-const playerSelection = require('./rps.js')
+const { TestWatcher } = require('jest')
+const playGame = require('./rps.js')
 const computerSelection = require('./rps.js')
 
 
@@ -8,6 +9,9 @@ test('properly adds two numbers', () => {
   expect(add(1, 2)).toBe(3)
 })*/
 
+
+/*Had to delete the tests because it was not testing what I wanted
+It was testing the selection but that is now built into the playGame Function
 //Player Selection Tests
 test('Player Selection correctly returns option' , () => {
   expect(playerSelection('rock')).toBe('rock')
@@ -25,5 +29,34 @@ test('Player Selection correctly returns option' , () => {
   expect(playerSelection('Cabbage')).toBe("Please select either 'Rock', 'Paper' or 'Scissors'")
 })
 
-//Computer Selection Tests
+*/
 
+//playGame Tests
+//Don't know enough about tests to be able to compare the values
+// Think I've bodged it a bit
+
+test("Game is a draw when player selection and computer selection are the same" , () => {
+  expect(playGame("Rock", "Rock")).toBe("Game is a Draw")
+})
+
+//Player winner tests
+test("Player Rock beats Computer Scissors" , () => {
+  expect(playGame("Rock", "Scissors")).toBe("Player chose Rock Computer chose Scissors Player Wins")
+})
+test("Player Scissors beats Computer Paper" , () => {
+  expect(playGame("Scissors", "Paper")).toBe("Player chose Scissors Computer chose Paper Player Wins")
+})
+test("Player Paper beats Computer Rock" , () => {
+  expect(playGame("Paper", "Rock")).toBe("Player chose Paper Computer chose Rock Player Wins")
+})
+
+//Computer Winner Tests
+test("Computer Rock beats Player Scissors" , () => {
+  expect(playGame("Scissors", "Rock")).toBe("Player chose Scissors Computer chose Rock Computer Wins")
+})
+test("Computer Scissors beats Player Paper" , () => {
+  expect(playGame("Paper", "Scissors")).toBe("Player chose Paper Computer chose Scissors Computer Wins")
+})
+test("Computer Paper beats Player Rock" , () => {
+  expect(playGame("Rock", "Paper")).toBe("Player chose Rock Computer chose Paper Computer Wins")
+})
