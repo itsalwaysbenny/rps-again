34ac5116191e74552b031f4c7cf60ef1ffccbee8
Ben Taylor <taylor.ben89@gmail.com>
2022-02-21 17:39:52 +0000

    Created the score reporting inside playGame
    It now is just in once rather than being copied after each if/else option

--- rps.js
+++ rps.js
@@ -23,29 +23,38 @@ let playerScore = 0;
 let computerScore = 0;
 let totalScore = playerScore + computerScore;
 
+
 function playRound(playerSelection, computerSelection) {
   let computerChoice = computerSelection.toLowerCase();
   let playerChoice = playerSelection.toLowerCase();
+  let computerWin = `Player chose ${playerSelection},
+  Computer chose ${computerSelection}, Computer Wins.
+  Player Score: ${playerScore} Computer Score: ${computerScore}`
+  let playerWin = `Player chose ${playerSelection},
+  Computer chose ${computerSelection}, Player Wins.
+  Player Score: ${playerScore} Computer Score: ${computerScore}`
   if (playerChoice === computerChoice) {
-    return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Game is a Draw. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    return `Player chose ${playerSelection},
+      Computer chose ${computerSelection}, Game is a Draw.
+       Player Score: ${playerScore} Computer Score: ${computerScore}`;
   } else if ((playerChoice === "rock") && (computerChoice === "scissors")) {
       playerScore++
-      return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Player Wins. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      return playerWin;
   } else if ((playerChoice === "scissors") && (computerChoice === "paper")) {
       playerScore++
-      return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Player Wins. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      return playerWin;
   } else if ((playerChoice === "paper") && (computerChoice === "rock")) {
       playerScore++
-      return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Player Wins. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      return playerWin;
   } else if ((playerChoice === "scissors") && (computerChoice === "rock")) {
       computerScore++
-      return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Computer Wins. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      return computerWin;
   } else if ((playerChoice === "paper") && (computerChoice === "scissors")) {
       computerScore++
-      return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Computer Wins. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      return computerWin;
   } else if ((playerChoice === "rock") && (computerChoice === "paper")) {
       computerScore++
-      return `Player chose ${playerSelection}, Computer chose ${computerSelection}, Computer Wins. Player Score: ${playerScore} Computer Score: ${computerScore}`;
+      return computerWin;
   } else {
     return "Please select either 'Rock', 'Paper' or 'Scissors'"
   }
